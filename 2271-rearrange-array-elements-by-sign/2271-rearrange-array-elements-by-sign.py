3f30098602355b755f6f7edea222e4ce2e6e4278
JITESH SUTHAR <106002247+JiteshSuthar-JS@users.noreply.github.com>
2025-06-11 10:55:08 +0530

    Time: 35 ms (91.62%), Space: 44 MB (19.08%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -9,22 +9,9 @@ class Solution:
             else:
                 neg.append(num)
         
-        ans = []
-        i = 0
-        j = 0
-         
-        while i < len(pos) and j < len(neg):
-            ans.append(pos[i])
-            ans.append(neg[j])
-            i += 1
-            j += 1
-         
-        while i < len(pos):
-            ans.append(pos[i])
-            i += 1
- 
-        while j < len(neg):
-            ans.append(neg[j])
-            j += 1
+        lst = []
+        for i,j in zip(pos,neg):
+            lst.append(i)
+            lst.append(j)
         
-        return ans
+        return lst
